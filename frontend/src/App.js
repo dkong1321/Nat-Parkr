9d70fe7749d12c4f016a1910994ffa49c1943591
darren <dkong1321@gmail.com>
2022-04-28 10:53:10 -0700

    session signup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 // frontend/src/App.js
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
+import * as sessionActions from "./store/session";
+import { useDispatch } from "react-redux";
+
+
+
 
 function App() {
-  return (
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  return isLoaded &&(
     <Switch>
       <Route path="/login">
         <LoginFormPage />
