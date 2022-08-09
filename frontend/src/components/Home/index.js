3e3319162bf6f861a188615dcbe3a570a7cebc6a
darren <dkong1321@gmail.com>
2022-08-09 15:04:48 -0700

    edit splash

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -8,6 +8,8 @@ function HomePage() {
                 {/* <img className='intro_logo' src="./images/Nat_Parkr_Logo_v2.svg"></img> */}
                 <div className='splash_intro_text'>Preserve, share, and explore the beauty of America's National Parks</div>
                 <div className='splash_intro_text_sub'>Join Nat-Parkr to start exploring today</div>
+                <br></br>
+
             </div>
             <div className="splash_text_container">
                 <div className='small_home_text'>
@@ -18,7 +20,7 @@ function HomePage() {
                 </div>
             </div>
 
-            <div className='footer_splash'>
+            <div className="profile_links">
                 <a href="https://github.com/dkong1321/PokeMart" target="blank">
                     <img className="svg__logo__git" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
                 </a>
@@ -26,6 +28,14 @@ function HomePage() {
                     <img className="svg__logo__linked" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" />
                 </a>
             </div>
+            {/* <div className='footer_splash'>
+                <a href="https://github.com/dkong1321/PokeMart" target="blank">
+                    <img className="svg__logo__git" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
+                </a>
+                <a href="https://www.linkedin.com/in/darren-kong-06b47013b/" target="blank">
+                    <img className="svg__logo__linked" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" />
+                </a>
+            </div> */}
         </div>
     )
 }
