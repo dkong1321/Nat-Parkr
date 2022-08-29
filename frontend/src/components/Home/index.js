33c859d96abe37d4da64d30c7ab6c9d8ea4c0e31
darren <dkong1321@gmail.com>
2022-08-29 10:18:34 -0700

    slight splash changes

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -6,36 +6,15 @@ function HomePage() {
         <div className='splash_container'>
             <div className='intro'>
                 {/* <img className='intro_logo' src="./images/Nat_Parkr_Logo_v2.svg"></img> */}
+                <div className='large_home_text'>
+                    Welcome to Nat-Parkr
+                </div>
                 <div className='splash_intro_text'>Preserve, share, and explore the beauty of America's National Parks</div>
                 <div className='splash_intro_text_sub'>Join Nat-Parkr to start exploring today</div>
                 <br></br>
 
             </div>
-            <div className="splash_text_container">
-                <div className='small_home_text'>
-                    Welcome to
-                </div>
-                <div className='large_home_text'>
-                    Nat-Parkr
-                </div>
-            </div>
 
-            <div className="profile_links">
-                <a href="https://github.com/dkong1321/PokeMart" target="blank">
-                    <img className="svg__logo__git" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
-                </a>
-                <a href="https://www.linkedin.com/in/darren-kong-06b47013b/" target="blank">
-                    <img className="svg__logo__linked" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" />
-                </a>
-            </div>
-            {/* <div className='footer_splash'>
-                <a href="https://github.com/dkong1321/PokeMart" target="blank">
-                    <img className="svg__logo__git" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
-                </a>
-                <a href="https://www.linkedin.com/in/darren-kong-06b47013b/" target="blank">
-                    <img className="svg__logo__linked" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" />
-                </a>
-            </div> */}
         </div>
     )
 }
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -16,14 +16,23 @@ function Navigation({ isLoaded }){
         <>
         <div className='nav_bar'>
                 <NavLink to="/" className="logo_container">
-                {/* <img className='nav_logo' src="./images/NatParkr.svg"></img> */}
-
                     <img className='nav_logo' src="/images/Nat_Parkr_Logo_v2.svg"></img>
                     <div className='logo_text'>Nat-Parkr</div>
-
-
                 </NavLink>
+
+
+                {sessionUser?
+                <a href="https://github.com/dkong1321/PokeMart" target="blank">
+                    <img className="svg__logo__git" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
+                </a>:<></>
+                }
+                {sessionUser?
+                <a href="https://www.linkedin.com/in/darren-kong-06b47013b/" target="blank">
+                    <img className="svg__logo__linked" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" />
+                </a>:<></>
+                }
                 {sessionUser ? <Search/>:<></>}
+
                 <div className='nav_bar_right'>
                 {!sessionUser?<LoginFormModal />:<></>}
                 {!sessionUser?<SignupFormModal/>:<></>}
